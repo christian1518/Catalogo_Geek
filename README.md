a8a887c261bca3c49dc52cba6dcf981a1c1036d2
rodrigues <crsantanarj@hotmail.com>
2023-03-14 19:58:51 -0300

    Entrega: Catálogo Geek

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+Entrega: Catálogo Geek
+A entrega consiste em um página web, que apresenta como catálogo duas seções, uma seção de "Paintings" associada a quadros decorativos, e uma seção de "Action Figures" associada a bonecos de ação. Inicialmente, cada seção possui 3 cards, ambos os cards possuem informações(imagem, nome, e valor) referente ao quadro decorativo ou boneco de ação.
+
+Na parte superior da página contem um bloco de navegação, onde constam o logo da loja, e dois butões, os quais mudam de cor, quando são clicados.
 
--- script.js
+++ script.js
@@ -35,4 +35,91 @@ let itens = [
         price: 'R$100,00',
         type: 'Action Figures'
     }
-];
+]
+
+
+const body = document.querySelector("body")
+
+//criando elementos e adicionando dados a eles
+const main = document.createElement("main")
+body.appendChild(main)
+
+const sectionFrames = document.createElement("section")
+const sectionAction = document.createElement("section") 
+main.append(sectionFrames, sectionAction)
+
+
+const titleFigures = document.createElement("h3")
+titleFigures.innerText = "Action Figures"
+sectionAction.appendChild(titleFigures)
+
+const titleFrames = document.createElement("h3")
+titleFrames.innerText = "Paintings"
+sectionFrames.appendChild(titleFrames)
+
+
+sectionAction.classList.add("action_section")
+sectionFrames.classList.add("frames_section")
+const ulFrames = document.createElement("ul")  // armazenará os cards tipo "frames"
+const ulFigures = document.createElement("ul")  // armazenará os cards tipo "action"
+sectionFrames.appendChild(ulFrames)  
+sectionAction.appendChild(ulFigures)
+
+
+
+let listFigures = []   //armazenará os objetos tipo "action"
+let listFrames = []    //armazenará os objetos tipo "frames"
+function separeteItens(itensList){ 
+    for(let i = 0; i < itensList.length; i++){
+        if(itensList[i].type == 'Painting'){
+            listFrames.push(itensList[i])
+        }else{
+            listFigures.push(itensList[i])
+        }
+    }
+}
+separeteItens(itens)
+
+
+//criando card para seção de frames
+for(let i = 0; i < listFrames.length; i++){
+    const cardFrames = document.createElement("li")
+
+    const boxImgFrames = document.createElement("figure")
+    const imgFrames = document.createElement("img")
+    imgFrames.src= listFrames[i].image
+    boxImgFrames.appendChild(imgFrames)
+    cardFrames.appendChild(boxImgFrames)
+
+    const nomeFrames = document.createElement("span")
+    nomeFrames.innerHTML = listFrames[i].name
+    cardFrames.appendChild(nomeFrames)
+
+    const priceFrames = document.createElement("p")
+    priceFrames.innerText = listFrames[i].price
+    cardFrames.appendChild(priceFrames)
+
+    ulFrames.appendChild(cardFrames)
+}
+
+
+//criando card para seção de action figures
+for(let i = 0; i < listFigures.length; i++){
+    const cardFigures = document.createElement("li")
+    
+    const boxImgFigures = document.createElement("figure")
+    const imgFigures = document.createElement("img")
+    imgFigures.src= listFigures[i].image
+    boxImgFigures.appendChild(imgFigures)
+    cardFigures.appendChild(boxImgFigures)
+
+    const nomeFigures = document.createElement("span")
+    nomeFigures.innerText = listFigures[i].name
+    cardFigures.appendChild(nomeFigures) 
+
+    const priceFigures = document.createElement("p")
+    priceFigures.innerText = listFigures[i].price
+    cardFigures.appendChild(priceFigures)
+
+    ulFigures.appendChild(cardFigures) 
+}
